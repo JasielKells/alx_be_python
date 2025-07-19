f80eb554aa9b7aef6fd7ab9cae295462b804546b
JasielKells <kellyosasng@gmail.com>
2025-07-19 23:32:31 +0100

    Performed Hours to Seconds conversion in hours_to_seconds.py and Future age calculation in future_age_calculator.py

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+present_age = int(input("How old are you?: "))
+current_year = 2023
+future_year = 2050
+future_age = present_age + future_year - current_year
+print("In 2050, you will be", future_age, "years old.")
